89e97cdbd56910c2d9c5f14be7b5a4fafc214809
ygdstmidn <2022d12@stu.gifu-nct.ac.jp>
2024-12-26 17:03:58 +0900

    ✨ printfの導入

--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -5,13 +5,25 @@ extern "C"
 {
 #endif
 
-void user_setup(void)
-{
-}
+#include "main.h"
 
-void user_loop(void)
-{
-}
+    extern UART_HandleTypeDef huart2;
+
+    void user_setup(void)
+    {
+        setbuf(stdout, NULL);
+        printf("Hello World!!\n");
+    }
+
+    void user_loop(void)
+    {
+    }
+
+    int _write(int file, char *ptr, int len)
+    {
+        HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, 10);
+        return len;
+    }
 
 #ifdef __cplusplus
 }
