51d4735c14f9e86a2397333285ce25e8921a710d
ygdstmidn <2022d12@stu.gifu-nct.ac.jp>
2024-12-26 17:03:17 +0900

    🐛 コンパイルが通らないため，extern "C" 等の追加

--- Core/Inc/app.h
+++ Core/Inc/app.h
@@ -1,7 +1,16 @@
 #ifndef __APP_H
 #define __APP_H
 
-void user_setup(void);
-void user_loop(void);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void user_setup(void);
+    void user_loop(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __APP_H
--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -1,5 +1,10 @@
 // main code here!!
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 void user_setup(void)
 {
 }
@@ -7,3 +12,7 @@ void user_setup(void)
 void user_loop(void)
 {
 }
+
+#ifdef __cplusplus
+}
+#endif
