31a4e7e06bf48abcf618e8f58b93d43739ec06c8
ygdstmidn <2022d12@stu.gifu-nct.ac.jp>
2026-07-10 23:05:21 +0900

    🎨 フォーマット

--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -24,7 +24,6 @@ extern "C"
 
         if (now - pre >= 10)
         {
-
             static int count = 0;
             count++;
             if (count >= 10)
@@ -43,9 +42,9 @@ extern "C"
     }
 
     // MARK:_write (for printf)
-    int _write(int file, char *ptr, int len)
+    int _write(int file, char* ptr, int len)
     {
-        HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, 10);
+        HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, 10);
         return len;
     }
 
